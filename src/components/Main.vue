1415ce593486d855fa14b7017ce549f558feec4e
nhinojosa <nhinojosa@southtexascollege.edu>
2023-09-02 12:27:15 -0500

    added files

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,20 @@
 <script setup>
 import MainSearch from './MainSearch.vue'
-
+import MainCards from './MainCards.vue'
 </script>
 
 
 <template>
     <div class="wrapper">
     <MainSearch />
-    <h1>Hello There</h1>
+    <MainCards />
     </div>
 </template>
 
 
 <style scoped lang="postcss">
 .wrapper {
-    @apply flex flex-col mx-auto py-4 gap-4;
+    @apply container mx-auto flex flex-col py-4 gap-4;
 }
 
 
---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <span class=" bg-red-500">Im a card</span>
+    </div>
+    
+</template>
