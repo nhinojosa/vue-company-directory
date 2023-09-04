0dd228fc77f566404bc2d259c800ef228aa59e8e
nhinojosa <nhinojosa@southtexascollege.edu>
2023-09-04 18:29:56 -0500

    Added .env file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import { useAuth } from '@/composables/useAuth'
 const {isAuthenticated, logout, user} = useAuth()
-    const brand = ref('🏛️ Fake Company Directory')
+    const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
 
